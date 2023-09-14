c75e74ca80527f9895e2252e4294bf53b10231f0
Stefan Elmlund <stefan.elmlund@saabgroup.com>
2023-09-14 10:05:35 +0200

    Support trim, collapse and xml:space

--- main.c
+++ main.c
@@ -6,8 +6,12 @@
 // TODO: Support escape signs (done)
 // TODO: Allocate the stack and xml_t block as the same.
 // TODO: Ignore <!DOCTYPE ... > tags. (done)
-// TODO: Support trim, collapse and xml:space
-// TODO: Create custom function for strcmp, itoaand toupper.
+// TODO: Support trim, collapse and xml:space (done)
+// TODO: Create custom function for strcmp, itoa and toupper.
+// TODO: Function document
+// TODO: Add to a header file.
+// TODO: README.md
+// TODO: Test with different xml files.
 
 typedef struct xml__impl xml_t;
 
@@ -32,6 +36,14 @@ const char* xml_get_text(xml_t* xml);
 
 const char* xml_get_error(xml_t* xml);
 
+int xml_get_trim(xml_t* xml);
+
+int xml_get_collapse(xml_t* xml);
+
+void xml_set_trim(xml_t* xml, int enable);
+
+void xml_set_collapse(xml_t* xml, int enable);
+
 void xml_close(xml_t* xml);
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -39,12 +51,16 @@ void xml_close(xml_t* xml);
 ///////////////////////////////////////////////////////////////////////////////
 
 #define STACK_SIZE (4096)
+#define XML_SPACE_STACK_SIZE (32)
 #define LABEL(addr) do{case addr:;}while(0);
 #define JMP(addr) do{xml->lc=addr;goto jp;}while(0)
 #define CALL(ret,addr) do{xml->lc=addr;xml__push(xml,&(enum xml__label){ret},sizeof(enum xml__label));goto jp;case ret:;}while(0)
 #define RET() do{xml->lc=*(enum xml__label*)xml__pop(xml, sizeof(enum xml__label));goto jp;}while(0);
 #define TOK(addr,tok) do{xml->lc=addr;return tok;case addr:;}while(0)
 #define NEXTCH() do{if(!xml__nextch(xml)) JMP(xml__error_loop);}while(0)
+#define FLAG_TRIM (0)
+#define FLAG_COLLAPSE (1)
+#define FLAG_PRESERVE (2)
 
 enum xml__label {
 	xml__start,
@@ -57,7 +73,7 @@ enum xml__label {
 	xml__error, xml__error_loop,
 	xml__c1, xml__c2, xml__c3, xml__c4, xml__c5, xml__c6, xml__c7, xml__c8, xml__c9, xml__c10,
 	xml__c11, xml__c12, xml__c13, xml__c14, xml__c15, xml__c16, xml__c17, xml__c18, xml__c19,
-	xml__c20, xml__c21, xml__c22, xml__c23,
+	xml__c20, xml__c21, xml__c22, xml__c23, xml__c24,
 	xml__l1, xml__l2, xml__l3, xml__l4,
 	xml__t1, xml__t2, xml__t3, xml__t4, xml__t5, xml__t6, xml__t7, xml__t8
 };
@@ -67,10 +83,15 @@ const char xml__error_while_reading_file[] = "Error: While reading file, code: "
 const char xml__error_prefix[] = "Error(";
 const char xml__unexpected_sign[] = "): Unexpected sign.";
 
+struct xml__xml_space {
+	int level, preserve;
+};
+
 struct xml__impl {
 	FILE* fp;
 	enum xml__label lc;
-	int ch, ra, rb, rc, row, col, sc;
+	int ch, ra, rb, rc, row, col, sc, level, flags, xml_space_count;
+	struct xml__xml_space xml_space_stack[XML_SPACE_STACK_SIZE];
 	uint8_t* stack;
 };
 
@@ -185,6 +206,9 @@ xml_t* xml_fopen(const char* filename)
 	xml->row = 1;
 	xml->sc = 0;
 	xml->fp = fp;
+	xml->level = 0;
+	xml->flags = (1 << FLAG_TRIM) | (1 << FLAG_COLLAPSE);
+	xml->xml_space_count = 0;
 
 	return xml;
 }
@@ -386,17 +410,53 @@ jp: switch (xml->lc) {
 			else JMP(xml__error);
 		}
 		CALL(xml__c13, xml__name);
+		xml->level++;
 		TOK(xml__t3, XML_TAG_START);
 		CALL(xml__c14, xml__padding);
 		while (xml->ch != '>' && xml->ch != '/') {
 			CALL(xml__c15, xml__attr);
-			TOK(xml__t4, XML_ATTRIBUTE);
+			if (strcmp(xml_get_name(xml), "xml:space") == 0) {
+				if (xml->xml_space_count > (XML_SPACE_STACK_SIZE - 1)) {
+					fprintf(stderr, "PANIC Maximum %d number of xml:space attribute have been reached.", XML_SPACE_STACK_SIZE);
+					exit(-1);
+				}
+				if (strcmp(xml_get_value(xml), "preserve") == 0) {
+					xml->xml_space_stack[xml->xml_space_count++] = (struct xml__xml_space){
+						.level = xml->level,
+						.preserve = (xml->flags & (1 << FLAG_PRESERVE)) > 0
+					};
+					xml->flags |= (1 << FLAG_PRESERVE);
+				}
+				else {
+					xml->flags &= ~(1 << FLAG_PRESERVE);
+					xml->xml_space_stack[xml->xml_space_count++] = (struct xml__xml_space){
+						.level = xml->level,
+						.preserve = (xml->flags & (1 << FLAG_PRESERVE)) > 0
+					};
+				}
+			}
+			else {
+				TOK(xml__t4, XML_ATTRIBUTE);
+			}
 			xml__pop_str(xml);
 			xml__pop_str(xml);
 			CALL(xml__c16, xml__padding);
 		}
 		if (xml->ch == '/') {
 			TOK(xml__t5, XML_TAG_END);
+			if (xml->xml_space_count > 0) {
+				struct xml__xml_space* xml_space = &xml->xml_space_stack[xml->xml_space_count - 1];
+				if (xml_space->level == xml->level) {
+					if (xml_space->preserve > 0 && xml->xml_space_count > 1) {
+						xml->flags |= (1 << FLAG_PRESERVE);
+					}
+					else {
+						xml->flags &= ~(1 << FLAG_PRESERVE);
+					}
+					xml->xml_space_count--;
+				}
+			}
+			xml->level--;
 			xml__pop_str(xml);
 			RET();
 		}
@@ -404,13 +464,35 @@ jp: switch (xml->lc) {
 		xml->ra = xml->sc;
 		LABEL(xml__tag_loop);
 		NEXTCH();
+		if (xml->flags & (1 << FLAG_TRIM) && ((xml->flags & (1 << FLAG_PRESERVE)) == 0)) {
+			CALL(xml__c24, xml__padding); // Padding
+		}
+		xml->rb = '\0';
 		while (xml->ch != '<') {
 			if (xml->ch == '&') {
 				CALL(xml__c23, xml__esc);
+				xml->rb = *(uint8_t*)xml__peek(xml, sizeof(uint8_t), 0);
 			}
 			else {
-				xml__push(xml, &(uint8_t){ xml->ch }, sizeof(uint8_t));
+				if (xml->flags & (1 << FLAG_COLLAPSE) && ((xml->flags & (1 << FLAG_PRESERVE)) == 0)) {
+					if (xml->ch == '\n' || xml->ch == '\r' || xml->ch == '\t') {
+						xml->ch = ' ';
+					}
+					if (!(xml->ch == ' ' && xml->rb == ' ')) {
+						xml__push(xml, &(uint8_t){ xml->ch }, sizeof(uint8_t));
+					}
+					xml->rb = xml->ch;
+				}
+				else {
+					xml__push(xml, &(uint8_t){ xml->ch }, sizeof(uint8_t));
+				}
 				NEXTCH();
+			}
+		}
+		if (xml->flags & (1 << FLAG_TRIM) && ((xml->flags & (1 << FLAG_PRESERVE)) == 0) && xml->sc != xml->ra) {
+			char ch = xml->stack[xml->sc - 1];
+			while (xml->sc > 0 && (ch == ' ' || ch == '\n' || ch == '\r' || ch == '\f' || ch == '\t')) {
+				ch = xml->stack[--(xml->sc) - 1];
 			}
 		}
 		NEXTCH();
@@ -423,6 +505,19 @@ jp: switch (xml->lc) {
 			NEXTCH();
 			CALL(xml__c18, xml__name);
 			TOK(xml__t7, XML_TAG_END);
+			if (xml->xml_space_count > 0) {
+				struct xml__xml_space* xml_space = &xml->xml_space_stack[xml->xml_space_count - 1];
+				if (xml_space->level == xml->level) {
+					if (xml_space->preserve > 0 && xml->xml_space_count > 1) {
+						xml->flags |= (1 << FLAG_PRESERVE);
+					}
+					else {
+						xml->flags &= ~(1 << FLAG_PRESERVE);
+					}
+					xml->xml_space_count--;
+				}
+			}
+			xml->level--;
 			xml__pop_str(xml);
 			CALL(xml__c17, xml__padding);
 			if (xml->ch != '>') JMP(xml__error);
@@ -558,6 +653,36 @@ const char* xml_get_text(xml_t* xml)
 		return &xml->stack[xml->sc - cnt - sizeof(int) - sizeof(uint8_t)];
 	}
 	return NULL;
+}
+
+int xml_get_trim(xml_t* xml)
+{
+	return (xml->flags & (1 << FLAG_TRIM)) > 0;
+}
+
+int xml_get_collapse(xml_t* xml)
+{
+	return (xml->flags & (1 << FLAG_COLLAPSE)) > 0;
+}
+
+void xml_set_trim(xml_t* xml, int enable)
+{
+	if (enable > 0) {
+		xml->flags |= 1 << FLAG_TRIM;
+	}
+	else {
+		xml->flags &= ~(1 << FLAG_TRIM);
+	}
+}
+
+void xml_set_collapse(xml_t* xml, int enable)
+{
+	if (enable > 0) {
+		xml->flags |= 1 << FLAG_COLLAPSE;
+	}
+	else {
+		xml->flags &= ~(1 << FLAG_COLLAPSE);
+	}
 }
 
 void xml_close(xml_t* xml)
